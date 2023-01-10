3173dd486fcb1d18a3088c4b855d50ef1989f41b
Arlaine Sanchez <amaesanchez@gmail.com>
2023-01-09 20:00:20 -0500

    add auth edits

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,9 @@ const Router = require("express").Router;
 const router = new Router();
 
 /** POST /login: {username, password} => {token} */
-
+router.post("/login", async function (req, res, next) {
+  const { username, password } = req.body
+})
 
 
 /** POST /register: registers, logs in, and returns token.
