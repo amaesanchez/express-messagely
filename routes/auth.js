1f639f22bc297fcbddc77a3ecc298d43e3efc794
Nico Martinucci <domenico.martinucci@gmail.com>
2023-01-10 10:10:21 -0800

    add login and register routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,26 @@
 "use strict";
 
+
 const Router = require("express").Router;
 const router = new Router();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const { UnauthorizedError, BadRequestError } = require("../expressError")
+
 
 /** POST /login: {username, password} => {token} */
 router.post("/login", async function (req, res, next) {
-  const { username, password } = req.body
+  if (!req.body || !req.body?.username || !req.body?.password) {
+    throw new BadRequestError("missing information - username/password required.")
+  }
+  
+  if (await User.authenticate(req.body)) {
+    const token = jwt.sign({ username: req.body.username }, SECRET_KEY);
+    return res.json({ token });
+  }
+
+  throw new UnauthorizedError("invalid credentials.");
 })
 
 
@@ -13,5 +28,12 @@ router.post("/login", async function (req, res, next) {
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+router.post("/register", async function (req, res, next) {
+  // const { username, password, first_name, last_name, phone } = req.body;
+  const user = await User.register(req.body);
+  const token = jwt.sign({ username: user.username }, SECRET_KEY);
+
+  return res.json({ token });
+});
 
 module.exports = router;
